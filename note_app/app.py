13085a68ec4230f381a1be5d7d25eaa17407d327
Hoang Tu <hoang.tu@sjsu.edu>
2023-11-28 23:33:05 -0800

    Error don't use

--- note_app/app.py
+++ note_app/app.py
@@ -1,20 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from flask_bootstrap import Bootstrap
-from flask_migrate import Migrate
-from flask_login import LoginManager
-
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, HiddenField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your-secret-key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///notes.db'
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-login = LoginManager(app)
-login.login_view = 'login'
-login.init_app(app)
-bootstrap = Bootstrap(app)
+
+users = [
+    {'username': 'user1', 'email': 'user1@example.com', 'password': 'password1'},
+    {'username': 'user2', 'email': 'user2@example.com', 'password': 'password2'}
+]
 
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -25,3 +24,147 @@ class Note(db.Model):
     reminder = db.Column(db.DateTime)
     highlighted_text = db.Column(db.String(1000))
     reminder = db.Column(db.DateTime, nullable=True)
+
+class NoteForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    highlights = HiddenField()  
+    
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Register')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/api/notes', methods=['GET', 'POST'])
+def handle_notes():
+    print("handle_notes function called")
+    notes = Note.query.order_by(Note.created_at.desc()).all()
+    print("Notes to send:", notes) 
+    if request.method == 'POST':
+        data = request.json
+        print("Received data:", data)
+        form = NoteForm(meta={'csrf': False})
+        if form.validate_on_submit():
+            new_note = Note(
+                title=form.title.data, 
+                content=form.content.data,
+                highlights=form.highlights.data,
+                reminder=data.get('reminder')
+            )
+            db.session.add(new_note)
+            db.session.commit()
+            return jsonify({'message': 'Note added', 'id': new_note.id}), 201
+        else:
+            return jsonify({'errors': form.errors}), 400
+
+    notes = Note.query.order_by(Note.created_at.desc()).all()
+    return jsonify([
+        {'id': note.id, 'title': note.title, 'content': note.content, 
+         'created_at': note.created_at.isoformat(), 'highlights': note.highlights}
+        for note in notes
+    ])
+
+@app.route('/test')
+def test():
+    return 'Test successful!'
+
+@app.route('/register_acc', methods=['POST', 'GET'])
+def register_acc():
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        email = form.email.data
+        password = form.password.data
+
+        if User.query.filter_by(username=username).first() or User.query.filter_by(email=email).first():
+            return "Username or email already exists. Please choose different credentials."
+
+
+        new_user = User(username=username, email=email, password=password)  
+        # print(new_user)
+        db.session.add(new_user)
+        db.session.commit()
+
+        session['username'] = username  
+        return redirect(url_for('/dashboard'))  
+
+    return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+
+        user = User.query.filter_by(username=username, password=password).first()
+
+        if user:
+            session['username'] = username
+            return redirect(url_for('dashboard'))
+
+        return "Invalid login credentials. Please try again."
+
+    return render_template('login.html', form=form)
+
+@app.route('/log_out')
+def log_out():
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+@app.route('/dashboard')
+def dashboard():
+    if 'username' in session:
+        return render_template('dashboard.html', username=session['username'])
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/create_note', methods=['GET', 'POST'])
+def create_note():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        highlights = request.form.get('highlights')
+        reminder_str = request.form.get('reminder')
+
+        reminder = None
+        if reminder_str:
+            try:
+                reminder = datetime.strptime(reminder_str, '%Y-%m-%dT%H:%M')
+            except ValueError:
+                pass  
+
+        new_note = Note(title=title, content=content, highlighted_text=highlights, reminder=reminder)
+        db.session.add(new_note)
+        db.session.commit()
+
+        return redirect(url_for('dashboard'))
+
+    return render_template('create_note.html')
+
+
+with app.app_context():
+    db.drop_all()  
+    db.create_all() 
+
+if __name__ == '__main__':
+    app.run (debug = True)
